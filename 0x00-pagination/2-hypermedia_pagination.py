8c48abdff43dc92f7859ffbf7fef21c6fbb51fa2
MGHefny <mgenedhefny@gmail.com>
2024-08-17 23:16:18 +0300

    2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -43,7 +43,6 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """hyper data"""
         all_page = len(self.dataset() / page_size)
-        front, last = index_range(page, page_size)
         info = self.get_page(page, page_size)
         task_data = {
             "page_size": page_size if page_size <= len(info) else len(info),
