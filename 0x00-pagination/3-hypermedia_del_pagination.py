66852bcbdf7a7f0da9c2d700b70147d44d3c9558
MGHefny <mgenedhefny@gmail.com>
2024-08-18 22:06:02 +0300

    3

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        return info page
+        p_ info : is bage info
+        c_info : is count the data
+        n_ind : is the next index
+        """
+        info = self.indexed_dataset()
+        assert index >= 0 and index <= max(info.keys())
+        begin = index or 0
+        c_info = 0
+        n_ind = None
+        p_info = []
+        while x in info and c_info < page_size:
+            value = info[x]
+            if x >= begin:
+                p_info.append(value)
+                c_info += 1
+                x += 1
+
+        if c_info == page_size:
+            n_ind = x
+
+        re_info = {
+            "index": index,
+            "next_index": n_ind,
+            "page_size": len(p_info),
+            "data": p_info,
+        }
+        return re_info
