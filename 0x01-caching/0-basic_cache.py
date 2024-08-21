030a8401de6a8801eb9fb39d20f0b28e961e1fb5
MGHefny <mgenedhefny@gmail.com>
2024-08-21 20:29:07 +0300

    0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,25 +1,18 @@
-#!/usr/bin/env python3
-"""
-"""
+#!/usr/bin/python3
+
+"""  """
+
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """ """
 
-    def __init__(self):
-        """ """
-        BaseCaching.__init__(self)
-
     def put(self, key, item):
         """ """
-        if key is None or item is None:
-            pass
-        else:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         """ """
-        if key is not None and key in self.cache_data.keys():
-            return self.cache_data[key]
-        return None
+        return self.cache_data.get(key, None)
