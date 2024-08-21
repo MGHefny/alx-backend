e56808ba337d669329bc042a9aa347e12d743ead
MGHefny <mgenedhefny@gmail.com>
2024-08-21 21:38:17 +0300

    0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+'''#!/usr/bin/python3
 
 
 from base_caching import BaseCaching
@@ -14,11 +14,36 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """send function"""
         if key is None or item is None:
-            return
-        self.cache_data[key] = item
+            self.cache_data[key] = item
 
     def get(self, key):
         """reseved function"""
-        if key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        if key not in None:
+            return self.cache_data[key]
+        return None'''
+
+
+#!/usr/bin/python3
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache"""
+
+    def __init__(self):
+        """BasicCache"""
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        """
+        if key is not None:
+            return self.cache_data.get(key, None)
+        return None
