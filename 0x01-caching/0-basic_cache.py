2664c3246f9f6fbefe1c79cbe5c7bc41808b450c
MGHefny <mgenedhefny@gmail.com>
2024-08-21 21:21:36 +0300

    0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python3
+""" BasicCache module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Basic Cache Class """
+
+    def _init_(self):
+        super()._init_()
+
+    def put(self, key, item):
+        """ Add key, Item to cache.data """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ get item of key from cache.data """
+        if key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+"""
+#!/usr/bin/python3
 
 """ Task 0 """
 
@@ -21,4 +45,5 @@ class BasicCache(BaseCaching):
         """reseved function"""
         if key not in self.cache_data:
             return None
-        return self.cache_data[key]
+        return self.cache_data[key]"""
+        
