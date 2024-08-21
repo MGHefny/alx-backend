cd30fb00158fe893970f913e4f3deb74e21ee3d2
MGHefny <mgenedhefny@gmail.com>
2024-08-21 21:29:48 +0300

    0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,17 +7,18 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """base_cash function"""
-
     def __init__(self):
         """the super call"""
         super().__init__()
 
     def put(self, key, item):
-        """send function"""
+        """ send function """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
         """reseved function"""
+        if key not in self.cache_data:
+            return None
         return self.cache_data[key]
