63b02a23791a9d82592d0bbe5936fe81fc7679ef
MGHefny <mgenedhefny@gmail.com>
2024-08-21 20:08:40 +0300

    0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 0"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """basw_cash class"""
+
+    def put(self, key, item):
+        """Puts item in cache"""
+        if key is not None and item is not None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """cashed"""
+        return self.cache_data.get(key, None)
