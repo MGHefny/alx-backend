e45a1e44327f8f51485e6b5e9663c87470048af2
MGHefny <mgenedhefny@gmail.com>
2024-08-21 22:35:37 +0300

    2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -26,8 +26,8 @@ class LIFOCache(BaseCaching):
             data_len = len(self.cache_data)
             if data_len >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
                 print("DISCARD: {}".format(self.dic_ordr[0]))
-                del self.cache_data[self.dic_ordr[0]]
-                del self.dic_ordr[0]
+                del self.cache_data[self.dic_ordr[-1]]
+                del self.dic_ordr[-1]
             if key in self.dic_ordr:
                 del self.dic_ordr[self.dic_ordr.index(key)]
             self.dic_ordr.append(key)
