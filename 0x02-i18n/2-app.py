24fdde62a1f544ee61f0393993057b743f990d01
MGHefny <mgenedhefny@gmail.com>
2024-08-27 20:16:24 +0300

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
-"""
+""" app flask Task 2"""
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
 class Config:
-    """Represents a Flask Babel configuration.
-    """
+    """ conf flask """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -15,23 +13,20 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
+
+
+@app.route('/')
+def main_index() -> str:
+    """ index page """
+    return render_template('2-index.html')
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
-    """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    """ web site get locale """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
-    return render_template('2-index.html')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
