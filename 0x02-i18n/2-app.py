5d31d20a858328fa025166bdacd10f9d1ce6aac2
MGHefny <mgenedhefny@gmail.com>
2024-08-27 20:07:29 +0300

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,6 +22,7 @@ def main_index() -> str:
     return render_template('2-index.html')
 
 
+@babel.localeselector
 def get_locale() -> str:
     """ web site get locale """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
