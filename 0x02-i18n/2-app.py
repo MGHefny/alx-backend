9d4b2cfe736bc1889cd638521500883cfcfc73b9
MGHefny <mgenedhefny@gmail.com>
2024-08-27 19:30:59 +0300

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,7 +25,7 @@ def get_locale() -> str:
 @app.route('/')
 def main_index() -> str:
     """ index page """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
